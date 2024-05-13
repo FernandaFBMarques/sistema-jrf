b3dca8991211ed132f4e8f2842dce5148af21abf
FernandaFBMarques <mfernandafbmarques@gmail.com>
2024-05-13 20:40:50 -0300

    container da header e instalando stylled components

--- src/componentes/Header/index.js
+++ src/componentes/Header/index.js
@@ -1,15 +1,23 @@
-import './estilo.css'
 import Logo from '../logo'
 import OpcoesHeader from '../OpcoesHeader'
 import IconesHeader from '../IconesHeader'
+import styled from 'styled-components';
+
+const HeaderContainer = styled.header`
+
+    background-color: #FFF;
+    display: flex;
+    justify-content: center;
+  
+`
 
 function Header(){
     return(
-        <header className='App-header'>
+        <HeaderContainer>
             <Logo/>
             <OpcoesHeader/>
             <IconesHeader/>
-      </header>
+      </HeaderContainer>
     )
 }
 
