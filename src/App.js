de18831d88f02d7801f38afd098086affe3ff3b2
FernandaFBMarques <mfernandafbmarques@gmail.com>
2024-05-21 21:37:11 -0300

    criando path CRUD com o back

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Funcionarios from './componentes/Funcionarios';
 import Header from './componentes/Header';
 import Pesquisa from './componentes/Pesquisa';
 import UltimosLancamentos from './componentes/UltimosLancamentos';
 import styled from 'styled-components';
 
+// Importar as novas páginas
+import Home from './pages/Home';
+import Page1 from './pages/Page1';
+import Page2 from './pages/Page2';
+
 const AppContainer = styled.div`
-    width: 100vw;
-    height: 100vh;
-    background-image: linear-gradient(90deg, #002F52 45%, #326589 165%);
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-`
+  width: 100vw;
+  height: 100vh;
+  background-image: linear-gradient(90deg, #002F52 45%, #326589 165%);
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+`;
 
 function App() {
   return (
-    <AppContainer>
-      <Header />
-      <Pesquisa />
-      <UltimosLancamentos />
-      <Funcionarios/>
-    </AppContainer>
+    <Router>
+      <AppContainer>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/page1" element={<Page1 />} />
+          <Route path="/page2" element={<Page2 />} />
+          {/* Outras rotas podem ser adicionadas aqui */}
+        </Routes>
+      </AppContainer>
+    </Router>
   );
 }
 
