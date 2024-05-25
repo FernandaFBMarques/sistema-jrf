9801cfed03ab0a85789d3623bff4a4ef1903ae46
FernandaFBMarques <mfernandafbmarques@gmail.com>
2024-05-25 19:59:38 -0300

    ajuste em opcoes header apos reaculo da header e da home page

--- src/componentes/OpcoesHeader/index.js
+++ src/componentes/OpcoesHeader/index.js
@@ -1,30 +1,50 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
-const textoOpcoes = ['CATEGORIAS', 'FAVORITOS', 'CLIENTES']
+const textoOpcoes = [
+  { texto: 'FUNCIONARIOS', path: '/page1' },
+  { texto: 'PRODUTOS', path: '/page2' },
+  { texto: 'PEDIDOS', path: '/page3' },
+  { texto: 'CLIENTES', path: '/page4' }
+];
 
 const Opcao = styled.li`
-    font-size: 16px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    text-align: center;
-    height: 100%;
-    padding: 0 5px;
-    cursor: pointer;
-    min-width: 120px;
-`
+  font-size: 16px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  height: 100%;
+  padding: 0 5px;
+  cursor: pointer;
+  min-width: 120px;
+`;
+
 const Opcoes = styled.ul`
-    display: flex;
-`
+  display: flex;
+`;
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: inherit;
+
+  &:hover {
+    color: #000; /* Ajuste de cor ao passar o mouse */
+  }
+`;
 
 function OpcoesHeader() {
-    return(
-        <Opcoes>
-            { textoOpcoes.map( (texto) => (
-                <Opcao><p>{texto}</p></Opcao>
-            ) ) }
-     </Opcoes>
-    )
+  return (
+    <Opcoes>
+      {textoOpcoes.map(({ texto, path }) => (
+        <Opcao key={texto}>
+          <StyledLink to={path}>
+            <p>{texto}</p>
+          </StyledLink>
+        </Opcao>
+      ))}
+    </Opcoes>
+  );
 }
 
-export default OpcoesHeader
+export default OpcoesHeader;
