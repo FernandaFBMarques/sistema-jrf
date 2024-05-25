0395aada975c7b32d06443a2b490f888b8e37097
FernandaFBMarques <mfernandafbmarques@gmail.com>
2024-05-25 19:57:04 -0300

    ajuste

--- src/componentes/Funcionarios/index.js
+++ src/componentes/Funcionarios/index.js
@@ -2,22 +2,20 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function Funcionarios() {
-  const [funcionarios, setFuncionarios] = useState([]); // Estado para armazenar a lista de funcionários
+  const [funcionarios, setFuncionarios] = useState([]); 
 
   useEffect(() => {
-    // Função para buscar os funcionários do servidor
     const fetchFuncionarios = async () => {
       try {
-        // Substitua 'http://localhost:8080/funcionarios' pelo seu URL correto
         const response = await axios.get('http://localhost:8080/funcionarios');
-        setFuncionarios(response.data); // Atualiza o estado com a lista de funcionários
+        setFuncionarios(response.data); 
       } catch (error) {
         console.error('Erro ao buscar os funcionários:', error);
       }
     };
 
-    fetchFuncionarios(); // Chama a função ao carregar o componente
-  }, []); // O array vazio garante que o efeito é executado apenas uma vez após o componente ser montado
+    fetchFuncionarios(); 
+  }, []); 
 
   return (
     <div>
