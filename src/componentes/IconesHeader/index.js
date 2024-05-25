eb6d8b3f8fac90238606c2aa378019a63d131531
FernandaFBMarques <mfernandafbmarques@gmail.com>
2024-05-25 19:58:05 -0300

    ajuste em icones header apos reaculo da header e da home page

--- src/componentes/IconesHeader/index.js
+++ src/componentes/IconesHeader/index.js
@@ -1,26 +1,43 @@
-import perfil from '../../imagens/perfil.svg'
-import sacola from '../../imagens/sacola.svg'
+import { Link } from 'react-router-dom';
+import perfil from '../../imagens/perfil.svg';
+import sacola from '../../imagens/sacola.svg';
 import styled from 'styled-components';
 
-const icones = [perfil, sacola]
+const icones = [
+  { src: perfil, path: '/' }, 
+  { src: sacola, path: '/' }  
+];
 
 const Icone = styled.li`
-    margin-right: 40px;
-    width: 25px;
-`
+  margin-right: 40px;
+  width: 25px;
+`;
+
 const Icones = styled.ul`
-    display: flex;
-    align-items: center;
-`
+  display: flex;
+  align-items: center;
+`;
+
+const StyledLink = styled(Link)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-decoration: none;
+  color: inherit;
+`;
 
 function IconesHeader() {
-    return(
-        <Icones>
-            { icones.map( (icone) => (
-                <Icone><img src={icone}></img></Icone>
-            ))}
-        </Icones>
-    )
+  return (
+    <Icones>
+      {icones.map((icone, index) => (
+        <Icone key={index}>
+          <StyledLink to={icone.path}>
+            <img src={icone.src} alt="Ícone" />
+          </StyledLink>
+        </Icone>
+      ))}
+    </Icones>
+  );
 }
 
-export default IconesHeader
+export default IconesHeader;
