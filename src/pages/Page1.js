6e95d67abd5f1b77ae788c366f5862521c52fcdc
FernandaFBMarques <mfernandafbmarques@gmail.com>
2024-05-25 17:54:31 -0300

    adicao do dropdown de filtro por cargo

--- src/pages/Page1.js
+++ src/pages/Page1.js
@@ -1,14 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react'; 
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
+import { Dropdown } from 'primereact/dropdown';
 import InputField from '../componentes/Input/InputField.js';
 import MyButton from '../componentes/Mybutton.js';
-import 'primereact/resources/themes/saga-blue/theme.css'; // ou qualquer outro tema que você esteja usando
+import 'primereact/resources/themes/saga-blue/theme.css'; 
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
 import 'primeflex/primeflex.css';
 import '../styles/customStyles.css';
-
 
 function Page1() {
   const [cpf, setCpf] = useState('');
@@ -20,17 +20,34 @@ function Page1() {
   const [cidade, setCidade] = useState('');
   const [bairro, setBairro] = useState('');
   const [cargo, setCargo] = useState('');
-  const [funcionarios, setFuncionarios] = useState('');
+  const [funcionarios, setFuncionarios] = useState([]);
+  const [filteredFuncionarios, setFilteredFuncionarios] = useState([]);
   const [error, setError] = useState(null);
+  const [selectedCargo, setSelectedCargo] = useState('todos'); 
 
-  // Criar Funcionario
+  useEffect(() => {
+    handleGetFuncionarios();
+  }, []);
+
+  useEffect(() => {
+    filterFuncionariosByCargo();
+  }, [selectedCargo, funcionarios]);
+
+  const cargos = [
+    { label: 'Todos', value: 'todos' },
+    { label: 'Contador', value: 'Contador' },
+    { label: 'Equipe de Vendas', value: 'Equipe de Vendas' },
+    { label: 'Dependente', value: 'Dependente' },
+    { label: 'Outro', value: 'Outro' }
+  ];
+
   const handleCreateFuncionario = async () => {
     try {
       const response = await fetch('http://localhost:8080/funcionarios', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
-      },
+        },
         body: JSON.stringify({ cpf, nome, telefone, email, rua, numero, cidade, bairro, cargo }),
       });
       if (!response.ok) {
@@ -39,13 +56,12 @@ function Page1() {
       const data = await response.json();
       console.log(data);
       setError(null);
-      handleGetFuncionarios(); 
+      handleGetFuncionarios();
     } catch (error) {
       setError(error.message);
     }
   };
 
-  // Obter todos os Funcionarios
   const handleGetFuncionarios = async () => {
     try {
       const response = await fetch('http://localhost:8080/funcionarios');
@@ -54,6 +70,7 @@ function Page1() {
       }
       const data = await response.json();
       setFuncionarios(data);
+      setSelectedCargo('todos');
       setError(null);
       console.log(data);
     } catch (error) {
@@ -61,7 +78,6 @@ function Page1() {
     }
   };
 
-  // Atualizar Funcionario
   const handleUpdateFuncionario = async () => {
     try {
       const response = await fetch(`http://localhost:8080/funcionarios/${cpf}`, {
@@ -83,7 +99,6 @@ function Page1() {
     }
   };
 
-  // Deletar Funcionario
   const handleDeleteFuncionario = async () => {
     try {
       const response = await fetch(`http://localhost:8080/funcionarios/${cpf}`, {
@@ -94,57 +109,66 @@ function Page1() {
       }
       console.log(`Funcionario com CPF ${cpf} deletado com sucesso.`);
       setError(null);
-      handleGetFuncionarios(); 
+      handleGetFuncionarios();
     } catch (error) {
       setError(error.message);
     }
   };
+
+  const filterFuncionariosByCargo = () => {
+    if (selectedCargo !== 'todos') {
+      const filtered = funcionarios.filter(func => func.cargo === selectedCargo);
+      setFilteredFuncionarios(filtered);
+    } else {
+      setFilteredFuncionarios(funcionarios);
+    }
+  };  
 
   return (
     <div>
       <h1>Funcionários</h1>
       <h2>Editar ou Criar Funcionários</h2>
 
-      <div class="formgrid grid">
-        <div class="field col-15 md:col-3">
+      <div className="formgrid grid">
+        <div className="field col-12 md:col-3">
           <InputField id="nome" label="Nome" value={nome} onChange={setNome} />
         </div>
-        <div class="field col-15 md:col-3 ">
+        <div className="field col-12 md:col-3">
           <InputField id="cpf" label="CPF" value={cpf} onChange={setCpf} />
         </div>
-        <div class="field col-15 md:col-3">
+        <div className="field col-12 md:col-3">
           <InputField id="telefone" label="Telefone" value={telefone} onChange={setTelefone} />
         </div>
-        <div class="field col-15 md:col-3">
+        <div className="field col-12 md:col-3">
           <InputField id="email" label="Email" value={email} onChange={setEmail} />
         </div>
-        <div class="field col-15 md:col-3">
+        <div className="field col-12 md:col-3">
           <InputField id="rua" label="Rua" value={rua} onChange={setRua} />
-          </div>
-        <div class="field col-15 md:col-3">
+        </div>
+        <div className="field col-12 md:col-3">
           <InputField id="numero" label="Número" value={numero} onChange={setNumero} />
-          </div>
-        <div class="field col-15 md:col-3">
+        </div>
+        <div className="field col-12 md:col-3">
           <InputField id="cidade" label="Cidade" value={cidade} onChange={setCidade} />
-          </div>
-        <div class="field col-15 md:col-3">
+        </div>
+        <div className="field col-12 md:col-3">
           <InputField id="bairro" label="Bairro" value={bairro} onChange={setBairro} />
         </div>
-        <div class="field col-15 md:col-3">
+        <div className="field col-12 md:col-3">
           <InputField id="cargo" label="Cargo" value={cargo} onChange={setCargo} />
         </div>
       </div>
-  
+
       <div className="button-container">
         <MyButton label="Criar Novo Funcionario" onClick={handleCreateFuncionario} className="custom-button" />
         <MyButton label="Atualizar Funcionario" onClick={handleUpdateFuncionario} className="custom-button" />
         <MyButton label="Remover Funcionario" onClick={handleDeleteFuncionario} className="custom-button" />
       </div>
+      {error && <p style={{ color: 'red' }}>{error}</p>}
 
       <h2>Deletar Funcionário</h2>
-      <div class="formgrid grid">
-        
-        <div class="field col-15 md:col-3 ">
+      <div className="formgrid grid">
+        <div className="field col-12 md:col-3">
           <InputField id="cpf" label="CPF" value={cpf} onChange={setCpf} />
         </div>
       </div>
@@ -152,9 +176,13 @@ function Page1() {
       {error && <p style={{ color: 'red' }}>{error}</p>}
 
       <h2>Lista de Funcionários</h2>
-      <MyButton label="Listar todos os Funcionários" onClick={handleGetFuncionarios} className="custom-button" />
+      <div className="flex-container" style={{ display: 'flex', alignItems: 'center', gap: '1rem' }}>
+        <Dropdown value={selectedCargo} options={cargos} onChange={(e) => setSelectedCargo(e.value)} placeholder="Selecione um Cargo" />
+        <MyButton label="Listar todos os Funcionários" onClick={handleGetFuncionarios} className="custom-button" />
+      </div>
+
       <div>
-        <DataTable value={funcionarios} tableStyle={{ minWidth: '50rem' }}>
+        <DataTable value={filteredFuncionarios} tableStyle={{ minWidth: '50rem' }}>
           <Column field="cpf" header="CPF" />
           <Column field="nome" header="Nome" />
           <Column field="telefone" header="Telefone" />
@@ -171,4 +199,3 @@ function Page1() {
 }
 
 export default Page1;
-
