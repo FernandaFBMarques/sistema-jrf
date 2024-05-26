665215d8e06a48a8bba3c4b23add3b5ecb394433
FernandaFBMarques <mfernandafbmarques@gmail.com>
2024-05-25 21:47:41 -0300

    ajuste estetico no titulo da home page

--- src/componentes/Titulo/index.js
+++ src/componentes/Titulo/index.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const Titulo = styled.h2`
     width: 100%;
     padding: 30px 0;
-    background-color: #FFF;
+    background-color: linear-gradient(90deg, #002F52 45%, #326589 165%);
     color: ${props => props.cor || '#000'};
     font-size: ${props => props.tamanhoFonte || '18px'};
     text-align: ${props => props.alinhamento || 'center'};
